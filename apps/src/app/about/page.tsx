0551afa8ade349a0b893b5c0e4e5911d92d0403f
UBU11 <uvaisubu11@gmail.com>
2025-12-30 23:40:46 +0530

    feat: bg image

--- apps/src/app/about/page.tsx
+++ apps/src/app/about/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import Image from "next/image";
+import main from "@/public/main.png";
+
 export default function About() {
   return (
-    <>
-      <h1 className="bg-orange-500 min-w-screen min-h-screen">about</h1>
-    </>
+    <div className="bg-white min-w-screen min-h-screen">
+      <Image
+        src={main}
+        className="min-w-screen min-h-screen "
+        alt="Picture of the author"
+      />
+    </div>
   );
 }
