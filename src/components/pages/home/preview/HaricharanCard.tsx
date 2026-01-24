132c805dcc95bd41e8f0e10e109eba2cc87729e1
Gayathri-KS101 <gayathrikssrr@gmail.com>
2026-01-24 07:28:17 +0530

    feat: add favicon, fix: update routing in register buttons of haricharan and fejo cards

--- src/components/pages/home/preview/HaricharanCard.tsx
+++ src/components/pages/home/preview/HaricharanCard.tsx
@@ -27,7 +27,7 @@ const HaricharanCard: React.FC = () => (
       </p>
       <Link
         scroll={false}
-        href={"/event"}
+        href={"/events"}
         className="absolute bottom-3 md:bottom-15 bg-[#A41F22] text-white text-[5px] lg:text-xl font-bold uppercase px-1 py-1 lg:px-8 lg:py-3"
       >
         REGISTER
